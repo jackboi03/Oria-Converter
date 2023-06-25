aaa394d16f7ba4deb74dcc24496f951b87a0c0c0
Scot_Survivor <unconfigured@null.spigotmc.org>
2023-06-25 17:05:27 +0100

    Switch to function to get parser

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
                     datefmt='%m-%d %H:%M')
 logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser(
-    prog='Oria',
-    description='Convert ItemsAdder config to Oxaren config files (and vice versa)')
 
-parser.add_argument('input-dir', type=str, help='Input directory (eg: ./plugins/ItemsAdder)')
-parser.add_argument('output-dir', type=str, help='Output directory (eg: (./Oxaren)')
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='Oria',
+        description='Convert ItemsAdder config to Oxaren config files (and vice versa)')
 
-parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-parser.add_argument("-ox", "--ia-to-oxaren", help="Convert ItemsAdder config to Oxaren config files",
-                    action="store_true")
-parser.add_argument("-ia", "--oxaren-to-ia", help="Convert Oxaren config files to ItemsAdder config",
-                    action="store_true")
+    parser.add_argument('input-dir', type=str, help='Input directory (eg: ./plugins/ItemsAdder)')
+    parser.add_argument('output-dir', type=str, help='Output directory (eg: (./Oxaren)')
 
-args = parser.parse_args()
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-ox", "--ia-to-oxaren", help="Convert ItemsAdder config to Oxaren config files",
+                        action="store_true")
+    parser.add_argument("-ia", "--oxaren-to-ia", help="Convert Oxaren config files to ItemsAdder config",
+                        action="store_true")
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    logger.debug('Input directory: %s', args.input_dir)
+    logger.debug('Output directory: %s', args.output_dir)
